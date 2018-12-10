06b60c43e2bbff3f148435a5fc310e66b8e910b8
gl40517 <gl40517@yahoo.com>
2018-12-10 22:48:09 +0800

    first commit

---
+++ LPR.py
@@ -0,0 +1,295 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+from numpy.linalg import norm
+import keras
+
+DEFAUTH_CUT_THRESH = eval(input('DEFAUTH_CUT_THRESH='))
+DEFAUTH_ANGLE = eval(input('DEFAUTH_ANGLE='))
+DEFAULT_GAUSSIANBLUR_SIZE = 5
+SOBEL_SCALE = 1
+SOBEL_DELTA = 0
+SOBEL_DDEPTH = cv2.CV_16S
+SOBEL_X_WEIGHT = 1
+SOBEL_Y_WEIGHT = 0
+DEFAULT_MORPH_SIZE_WIDTH = 25
+DEFAULT_MORPH_SIZE_HEIGHT = 3
+DEFAULT_ERROR = 0.8
+DEFAULT_ASPECT = 3.75
+DEFAULT_LETTER_THRESHOLD = 0.06
+PROVINCES = ("京" ,"闽" ,"粤" ,"苏" ,"沪" ,"浙")
+LETTERS_DIGITS = (
+"A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+"I", "O")
+NUMBERS_DIGITS = (
+"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "P",
+"Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
+
+class StatModel(object):
+    def load(self, fn):
+        self.model = self.model.load(fn)
+    def save(self, fn):
+        self.model.save(fn)
+
+class SVM(StatModel):
+    def __init__(self, C = 1, gamma = 0.5):
+        self.model = cv2.ml.SVM_create()
+        self.model.setGamma(gamma)
+        self.model.setC(C)
+        self.model.setKernel(cv2.ml.SVM_RBF)
+        self.model.setType(cv2.ml.SVM_C_SVC)
+
+    def train(self, samples, responses):
+        self.model.train(samples, cv2.ml.ROW_SAMPLE, responses)
+#字符识别
+    def predict(self, samples):
+        r = self.model.predict(samples)
+        return r[1].ravel()
+
+def preprocess_hog(digits):
+    samples = []
+    for img in digits:
+        gx = cv2.Sobel(img, cv2.CV_32F, 1, 0)
+        gy = cv2.Sobel(img, cv2.CV_32F, 0, 1)
+        mag, ang = cv2.cartToPolar(gx, gy)
+        bin_n = 16
+        bin = np.int32(bin_n * ang / (2 * np.pi))
+        bin_cells = bin[:10, :10], bin[10:, :10], bin[:10, 10:], bin[10:, 10:]
+        mag_cells = mag[:10, :10], mag[10:, :10], mag[:10, 10:], mag[10:, 10:]
+        hists = [np.bincount(b.ravel(), m.ravel(), bin_n) for b, m in zip(bin_cells, mag_cells)]
+        hist = np.hstack(hists)
+
+        # transform to Hellinger kernel
+        eps = 1e-7
+        hist /= hist.sum() + eps
+        hist = np.sqrt(hist)
+        hist /= norm(hist) + eps
+
+        samples.append(hist)
+    return np.float32(samples)
+
+def rotateImage(img_roi):
+    #img_roi_gray = cv2.cvtColor(img_roi, cv2.COLOR_BGR2GRAY)
+    #img_roi_canny = cv2.Canny(img_roi_gray, threshold1=200, threshold2=400)
+    #lines = cv2.HoughLinesP(img_roi_canny,1,np.pi/180,20,minLineLength=20,maxLineGap=5)
+    #lines1 = lines[:,0,:]#提取为二维
+    #plt.figure(1)
+
+    #line_left, line_right, line_down, line_up = None, None, None, None
+    #for x1,y1,x2,y2 in lines1[:]:
+        #if x1>(img_roi.shape[1]*5/6) and x2>(img_roi.shape[1]*5/6):
+            #if abs(y1-y2) > img_roi.shape[0]/3:
+                #line_left, line_right, line_down, line_up = x1, x2, y1, y2
+    #if line_down > line_up:
+        #tan = (line_down - line_up)/(line_right-line_left)
+        #angle = 90 - (math.atan(tan) * 180 / math.pi)
+    center = (img_roi.shape[1]//2, img_roi.shape[0]//2)
+    rot = cv2.getRotationMatrix2D(center=center, angle=DEFAUTH_ANGLE, scale=1.0)
+    img_roi_rotated = cv2.warpAffine(img_roi, rot, (img_roi.shape[1], img_roi.shape[0]))
+    plt.imshow(img_roi_rotated[:,10:img_roi_rotated.shape[1]-10])
+    plt.show()
+    return img_roi_rotated
+
+def verifySizes(mr, img_width):
+    error = DEFAULT_ERROR
+    aspect = DEFAULT_ASPECT
+    rmin = aspect - aspect * error
+    rmax = aspect + aspect * error
+    angle = mr[2]
+    width, height = mr[1]
+    if (width == 0) or (height == 0):
+        return False
+    if width > height:
+        if width < img_width / 4:
+            return False
+    elif width < height:
+        if height < img_width / 4:
+            return False
+    r = width / height
+    if r < 1:
+        r = height / width
+
+    #if (area < Min or area > Max) or (r < rmin or r > rmax):
+    if r < rmin or r > rmax:
+        return False
+    elif -6 > angle > -84:
+        return False
+    else:
+        return True
+
+
+img = cv2.imread('LPR.jpg')
+img_blur = cv2.GaussianBlur(src=img,
+                            ksize=(DEFAULT_GAUSSIANBLUR_SIZE,DEFAULT_GAUSSIANBLUR_SIZE),
+                            sigmaX=0)
+img_gray = cv2.cvtColor(src=img_blur,code=cv2.COLOR_BGR2GRAY)
+grad_x = cv2.Sobel(src=img_gray,ddepth=SOBEL_DDEPTH,dx=1,
+                   dy=0,ksize=3,scale=SOBEL_SCALE,delta=SOBEL_DELTA)
+abs_grad_x = cv2.convertScaleAbs(src=grad_x)
+grad_y = cv2.Sobel(src=img_gray,ddepth=SOBEL_DDEPTH,dx=0,
+                   dy=1,ksize=3,scale=SOBEL_SCALE,delta=SOBEL_DELTA)
+abs_grad_y = cv2.convertScaleAbs(src=grad_y)
+grad = cv2.addWeighted(src1=abs_grad_x,alpha=SOBEL_X_WEIGHT,
+                       src2=abs_grad_y,beta=SOBEL_Y_WEIGHT,gamma=0)
+(retval, img_threshold) = cv2.threshold(src=grad,thresh=0,maxval=255,type=cv2.THRESH_OTSU+cv2.THRESH_BINARY)
+element = cv2.getStructuringElement(shape=cv2.MORPH_RECT,
+                                    ksize=(DEFAULT_MORPH_SIZE_WIDTH,DEFAULT_MORPH_SIZE_HEIGHT))
+img_threshold = cv2.morphologyEx(src=img_threshold,op=cv2.MORPH_CLOSE,
+                                 kernel=element)
+image, contours, hierarchy = cv2.findContours(image=img_threshold,
+                                              mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_NONE)
+contours = np.array(contours)
+rect = []
+img_height = img.shape[0]
+img_length = img.shape[1]
+for i in range(len(contours)):
+    mr = cv2.minAreaRect(contours[i])
+    if verifySizes(mr, img_length):
+        rect.append(mr)
+rect = np.array(rect)
+dst = np.zeros_like(img_threshold)
+result = []
+for i in range(rect.shape[0]):
+    flag = True
+    box = cv2.boxPoints(tuple(rect[i]))
+    box = np.int32(box)
+    for j in range(4):
+        if img_height*4/5 < box[j][1] or box[j][1] < img_height*2/5:
+            flag = False
+            break
+    Sum = 0
+    for j in range(4):
+        Sum += box[j][0]
+    if img_length*3/5 < Sum/4 or Sum/4 < img_height*2/5:
+        flag = False
+    if flag:
+        result.append(box)
+        for j in range(4):
+            cv2.line(dst, tuple(box[j]), tuple(box[(j+1) % 4]), (255,0,0), 5)
+w_left, h_up = np.min(result[0],axis=0)
+w_right, h_down = np.max(result[0],axis=0)
+img_roi = img[h_up+5:h_down-5 , w_left:w_right]
+img_roi_RGB = cv2.cvtColor(img_roi, cv2.COLOR_BGR2RGB)
+img_roi_HSV = cv2.cvtColor(img_roi, cv2.COLOR_BGR2HSV)
+blueLower = np.array([100, 80, 80])
+blueUpper = np.array([124, 255, 255])
+mask = cv2.inRange(img_roi_HSV, blueLower, blueUpper)
+mask_percentage_vertical = (np.sum(mask,axis=0)/255) / mask.shape[0]
+LPR_left = LPR_right = 0
+for i in range(len(mask_percentage_vertical)):
+    if mask_percentage_vertical[i] > 0.1:
+        LPR_left = i
+        break
+for i in range(len(mask_percentage_vertical)):
+    if mask_percentage_vertical[len(mask_percentage_vertical)-i-1] > 0.1:
+        LPR_right = len(mask_percentage_vertical)-i
+        break
+img_roi = img_roi[:,LPR_left-2 : LPR_right+2]
+img_roi = cv2.cvtColor(img_roi, cv2.COLOR_BGR2GRAY)
+
+MAX = img_roi.max()
+MIN = img_roi.min()
+img_linear_gray = np.uint8(255 / (MAX - MIN) * img_roi - 255 * MIN / (MAX - MIN))
+ret, img_roi_threadhold = cv2.threshold(img_linear_gray, DEFAUTH_CUT_THRESH, 255, cv2.THRESH_BINARY)
+#img_roi_threadhold = cv2.dilate(img_roi_threadhold, kernel=(2,2))
+img_roi_threadhold = rotateImage(img_roi_threadhold)
+
+
+percentage_vertical = (np.sum(img_roi_threadhold,axis=0)/255) / img_roi_threadhold.shape[0]
+def cut(percentage, thresh):
+    left = []
+    right = []
+    for i in range(len(percentage)):
+        if i == 0:
+            continue
+        if percentage[i-1] < thresh < percentage[i]:
+            left.append(i)
+        if percentage[i] < thresh < percentage[i-1]:
+            right.append(i)
+    return left,right
+roi_left,roi_right = cut(percentage_vertical,0.07)
+#print(roi_left,roi_right)
+final_result = []
+number = min(len(roi_left),len(roi_right))
+for i in range(number):
+    isOne = True
+    if roi_right[i] - roi_left[i] > (roi_right[len(roi_right)-1] - roi_left[0]) / 11:
+        final_result.append(img_roi_threadhold[:, int(roi_left[i]):int(roi_right[i])])
+    else:
+        aver = np.sum(percentage_vertical[roi_left[i]:roi_right[i]+1],axis=0) / (roi_right[i] - roi_left[i])
+        if aver > 0.6 or aver < 0.5:
+            isOne = False
+        if isOne:
+            final_result.append(img_roi_threadhold[:, int(roi_left[i]):int(roi_right[i])])
+
+#model = SVM(C=1, gamma=0.5)
+#modelChinese = SVM(C=1, gamma=0.5)
+#model.load("svm.dat")
+#modelChinese.load("svmchinese.dat")
+#SZ = 20
+model_Chinese = keras.models.load_model('.\chinese_character_model.h5')
+model_letter = keras.models.load_model('.\letter_model.h5')
+model_number = keras.models.load_model('./number_model.h5')
+predict_result = []
+for i in range(len(final_result)):
+    part_card = final_result[i][11:68, :]
+    w = abs(part_card.shape[1] - 32) // 2
+    part_card = cv2.copyMakeBorder(part_card, 0, 0, w, w, cv2.BORDER_CONSTANT, value=[0, 0, 0])
+    part_card = cv2.resize(part_card, (32,40),interpolation=cv2.INTER_AREA)
+    ret, part_card = cv2.threshold(part_card, 160, 255, cv2.THRESH_BINARY)
+    if i != 0:
+        kernel = np.ones((3, 3), np.uint8) #!!!!!试了一万年（一个小时）才试出来
+        part_card = cv2.dilate(part_card, kernel)
+    plt.imshow(part_card,'gray')
+    plt.show()
+    input_images = np.zeros(1280)
+    width = part_card.shape[1]
+    height = part_card.shape[0]
+    for h in range(height):
+        for w in range(width):
+            if part_card[h][w] > 190:
+                input_images[w + h * width] = 0
+            else:
+                input_images[w + h * width] = 1
+    input_images = np.reshape(input_images,(1,1280))
+    if i == 0:
+        proba_Chinese = model_Chinese.predict_classes(input_images, verbose=0)
+        predict_result.append(PROVINCES[proba_Chinese[0]])
+    elif i == 1:
+        proba_letter = model_letter.predict_classes(input_images, verbose=0)
+        predict_result.append(LETTERS_DIGITS[proba_letter[0]])
+    else:
+        proba_number = model_number.predict_classes(input_images, verbose=0)
+        print(proba_number[0])
+        predict_result.append(NUMBERS_DIGITS[proba_number[0]])
+'''
+for i in range(len(final_result)):
+    part_card = final_result[i][int(final_result[i].shape[0]/10) : int(final_result[i].shape[0]*4.3/5),:]
+    part_card_old = part_card
+    w = abs(part_card.shape[1] - SZ) // 2
+
+    part_card = cv2.copyMakeBorder(part_card, 0, 0, w, w, cv2.BORDER_CONSTANT, value=[0, 0, 0])
+    #plt.imshow(part_card,'gray')
+    #plt.show()
+    part_card = cv2.resize(part_card, (SZ, SZ), interpolation=cv2.INTER_AREA)
+    part_card = preprocess_hog([part_card])
+    if i == 0:
+        resp = modelChinese.predict(part_card)
+        charactor = provinces[int(resp[0]) - PROVINCE_START]
+    else:
+        resp = model.predict(part_card)
+        charactor = chr(resp[0])
+
+    predict_result.append(charactor)
+'''
+print(predict_result)
+
+#plt.figure(1)
+#plt.imshow(img_roi_RGB)
+#plt.figure(2)
+#plt.imshow(img_roi_threadhold,'gray')
+plt.figure(3)
+for i in range(len(final_result)):
+    plt.subplot(3,3,i+1)
+    plt.imshow(final_result[i],'gray')
+plt.show()
---
+++ make_letter_dataset.py
@@ -0,0 +1,150 @@
+import os
+import random
+import numpy as np
+from PIL import Image
+import keras
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation
+from keras.optimizers import RMSprop
+
+SIZE = 1280
+WIDTH = 32
+HEIGHT = 40
+NUM_CLASSES = 26
+batch_size = 64
+nb_epoch = 50
+LETTERS_DIGITS = (
+"A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+"I", "O")
+
+
+input_count = 0
+for i in range(0 + 10, NUM_CLASSES + 10):
+    dir = './tf_car_license_dataset/train_images/training-set/letters/%s/' % i  # 这里可以改成你自己的图片目录，i为分类标签
+    for rt, dirs, files in os.walk(dir):
+        for filename in files:
+            input_count += 1
+
+    # 定义对应维数和各维长度的数组
+input_images = np.array([[0] * SIZE for i in range(input_count)])
+input_labels = np.array([[0] * NUM_CLASSES for i in range(input_count)])
+
+    # 第二次遍历图片目录是为了生成图片数据和标签
+index = 0
+for i in range(0 + 10, NUM_CLASSES + 10):
+    dir = './tf_car_license_dataset/train_images/training-set/letters/%s/' % i  # 这里可以改成你自己的图片目录，i为分类标签
+    for rt, dirs, files in os.walk(dir):
+        for filename in files:
+            filename = dir + filename
+            img = Image.open(filename)
+            width = img.size[0]
+            height = img.size[1]
+            for h in range(0, height):
+                for w in range(0, width):
+                    # 通过这样的处理，使数字的线条变细，有利于提高识别准确率
+                    if img.getpixel((w, h)) > 230:
+                        input_images[index][w + h * width] = 0
+                    else:
+                        input_images[index][w + h * width] = 1
+            # print ("i=%d, index=%d" % (i, index))
+            input_labels[index][i - 10] = 1
+            index += 1
+
+    # 第一次遍历图片目录是为了获取图片总数
+val_count = 0
+for i in range(0 + 10, NUM_CLASSES + 10):
+    dir = './tf_car_license_dataset/train_images/validation-set/%s/' % i  # 这里可以改成你自己的图片目录，i为分类标签
+    for rt, dirs, files in os.walk(dir):
+        for filename in files:
+            val_count += 1
+
+# 定义对应维数和各维长度的数组
+val_images = np.array([[0] * SIZE for i in range(val_count)])
+val_labels = np.array([[0] * NUM_CLASSES for i in range(val_count)])
+
+# 第二次遍历图片目录是为了生成图片数据和标签
+index = 0
+for i in range(0 + 10, NUM_CLASSES + 10):
+    dir = './tf_car_license_dataset/train_images/validation-set/%s/' % i  # 这里可以改成你自己的图片目录，i为分类标签
+    for rt, dirs, files in os.walk(dir):
+        for filename in files:
+            filename = dir + filename
+            img = Image.open(filename)
+            width = img.size[0]
+            height = img.size[1]
+            for h in range(0, height):
+                for w in range(0, width):
+                    # 通过这样的处理，使数字的线条变细，有利于提高识别准确率
+                    if img.getpixel((w, h)) > 190:
+                        val_images[index][w + h * width] = 0
+                    else:
+                        val_images[index][w + h * width] = 1
+            val_labels[index][i - 10] = 1
+            index += 1
+X_train = input_images.astype('float32')
+X_test = val_images.astype('float32')
+# 归一化到0-1
+X_train /= 255
+X_test /= 255
+print(X_train.shape[0], 'train samples')
+print(X_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+# to_categorical(y, nb_classes=None)
+# 将类别向量(从0到nb_classes的整数向量)映射为二值类别矩阵, 用于应用到以categorical_crossentropy为目标函数的模型中.
+# y: 类别向量; nb_classes:总共类别数
+#Y_train = np_utils.to_categorical(input_labels, NUM_CLASSES)
+#Y_test = np_utils.to_categorical(val_labels, NUM_CLASSES)
+Y_train = input_labels
+Y_test = val_labels
+# Dense层:即全连接层
+# keras.layers.core.Dense(output_dim, init='glorot_uniform', activation='linear', weights=None, W_regularizer=None, b_regularizer=None, activity_regularizer=None, W_constraint=None, b_constraint=None, bias=True, input_dim=None)
+
+
+model = Sequential()
+model.add(Dense(512, input_shape=(1280,)))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+# 激活函数可以通过设置单独的激活层实现，也可以在构造层对象时通过传递activation参数实现。
+# 以下两行等价于：model.add(Dense(512,activation='relu'))
+model.add(Dense(512))
+model.add(Activation('relu'))
+
+# Dropout  需要断开的连接的比例
+model.add(Dropout(0.2))
+model.add(Dense(26))
+model.add(Activation('softmax'))
+
+# 打印出模型概况
+#print('model.summary:')
+#model.summary()
+
+# 在训练模型之前，通过compile来对学习过程进行配置
+# 编译模型以供训练
+# 包含评估模型在训练和测试时的性能的指标，典型用法是metrics=['accuracy']
+# 如果要在多输出模型中为不同的输出指定不同的指标，可像该参数传递一个字典，例如metrics={'ouput_a': 'accuracy'}
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=RMSprop(),
+              metrics=['accuracy'])
+
+# 训练模型
+# Keras以Numpy数组作为输入数据和标签的数据类型
+# fit(self, x, y, batch_size=32, nb_epoch=10, verbose=1, callbacks=[], validation_split=0.0, validation_data=None, shuffle=True, class_weight=None, sample_weight=None)
+# nb_epoch：整数，训练的轮数，训练数据将会被遍历nb_epoch次。Keras中nb开头的变量均为"number of"的意思
+# verbose：日志显示，0为不在标准输出流输出日志信息，1为输出进度条记录，2为每个epoch输出一行记录
+# shuffle：布尔值，表示是否在训练过程中每个epoch前随机打乱输入样本的顺序。
+
+# fit函数返回一个History的对象，其History.history属性记录了损失函数和其他指标的数值随epoch变化的情况，如果有验证集的话，也包含了验证集的这些指标变化情况
+History = model.fit(X_train, Y_train,
+                    batch_size=batch_size, epochs=nb_epoch,
+                    verbose=2, validation_data=(X_test, Y_test))
+
+
+# evaluate(self, x, y, batch_size=32, verbose=1, sample_weight=None)
+# 按batch计算在某些输入数据上模型的误差
+print('-------evaluate--------')
+score = model.evaluate(X_test, Y_test, verbose=0)
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
+model.save('.\letter_model.h5')
+
